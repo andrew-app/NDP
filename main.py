3ae4442b3d3db689538de72f489ce3077cc7f6cf
andrewapp <andrew.appuhamy@gmail.com>
2022-04-20 14:12:45 +1000

    busy server logic fix

--- main.py
+++ main.py
@@ -23,30 +23,33 @@ class event_sim:
         block_arrival = [False] * self.maxArrival
         service_finish_time = [0] * self.maxArrival
 
-        current_load = 0
+        
 
         avg_inter_arrival_time = 1/self.avg_arrival
 
+        for i in range(1,self.maxArrival):
+            inter_arrival_time = npr.exponential(avg_inter_arrival_time)
+            arrival_time.append(round(arrival_time[i-1] + inter_arrival_time,4))
+
         
         for i in range(0,self.maxArrival):
-            if i > 0:
-                inter_arrival_time = npr.exponential(avg_inter_arrival_time)
-                arrival_time.append(round(arrival_time[i-1] + inter_arrival_time,4))
-            
             service_duration[i] = round(npr.exponential(1/mu),4)
             service_finish_time[i] = round(arrival_time[i] + service_duration[i],4)
 
-            if i > 0 and service_finish_time[i-1] >= arrival_time[i]:
-                current_load += 1
+
+        for i in range(1,self.maxArrival):
+            current_load = 0
+            for j in range(i,0,-1):
+                if service_finish_time[j] >= arrival_time[i]:
+                    current_load += 1
             no_busy_server[i] = current_load
-            if no_busy_server[i] == int(self.k):
-                no_busy_server[i] = int(self.k)
+            if no_busy_server[i] == self.k:
                 block_arrival[i] = True
                 current_load = 0
                 service_finish_time[i] = 0
-
-
         no_block_arrival = block_arrival.count(True)
+
+        print(no_block_arrival)
 
         return no_block_arrival/self.maxArrival
         
@@ -62,15 +65,19 @@ if __name__ == "__main__":
     system_bpr = []
 
     mu = 1
-    simulation = event_sim(2000, k, k)#first simulation uses k as offered load value
+    simulation = event_sim(900, k, k)#first simulation uses k as offered load value
 
     offered_load.append(k)
     system_bpr.append(simulation.result())
 
-    for event in range (k-3,3,-3): #run 5 simulations decreasing offered load by 3 each time
+    it = 100
+
+    for event in range (k-3,3,-3): #run 5 simulations decreasing offered load(lambda) by 3 each time
+        simulation.maxArrival = 900 + it
         simulation.avg_arrival = event
         offered_load.append(event)
         system_bpr.append(simulation.result())
+        it = it + 100
 
     offered_load = offered_load[::-1]
 
@@ -91,7 +98,9 @@ if __name__ == "__main__":
 
     ax.set(xlabel='offered load(Erlang)', ylabel='system blocking probability',
         title="Blocking Probability of a M/M/k/k system")
-    ax.grid()
+
+    ax.set_yscale('log')
+    
 
     fig.savefig("results.png")
     plt.show()
