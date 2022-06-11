078c075845b5cc6c6c24a0d1aabb3d9c5346bef0
andrewapp <andrew.appuhamy@gmail.com>
2022-06-11 14:00:15 +1000

    part 3 complete

--- part_three/main.py
+++ part_three/main.py
@@ -15,19 +15,20 @@ if __name__ == "__main__":
     
     bpr = []
     k = math.floor(42/2)
-    lamda = 20
-    offered_load =[]
-    for i in range(0,6):
+    lamda = k
+    max_arrival = 100
+    offered_load = []
+    for sim in range(0,6):
         links = {
-        'l1' : 10,
-        'l2' : 15,
-        'l3' : 10,
-        'l4' : 20,
-        'l5' : 15,
-        'l6' : 20,
-        'l7' : 15,
-        'l8' : 15,
-        'l9' : 20
+        'l1' : [10,[0]*max_arrival],
+        'l2' : [15,[0]*max_arrival],
+        'l3' : [10,[0]*max_arrival],
+        'l4' : [20,[0]*max_arrival],
+        'l5' : [15,[0]*max_arrival],
+        'l6' : [20,[0]*max_arrival],
+        'l7' : [15,[0]*max_arrival],
+        'l8' : [15,[0]*max_arrival],
+        'l9' : [20,[0]*max_arrival]
         }
 
 
@@ -44,13 +45,11 @@ if __name__ == "__main__":
             10:['l1','l2']
         }
         
-        max_arrival = 13
 
         arrival_time = [0] * max_arrival
         service_duration = [0] * max_arrival
-        no_busy_server = [0] * max_arrival
+        
         block_accept = [False] * max_arrival
-        service_finish_time = [0] * max_arrival
 
         arrival_time[0] = 1
         avg_inter_arrival_time = 1/lamda
@@ -61,32 +60,36 @@ if __name__ == "__main__":
         
         for i in range(0,max_arrival):
             service_duration[i] = npr.exponential((1/mu))
+        
 
-        no_busy_server[0] = 0
-
+        current_path = random.randint(1, 10)
+        
         block_accept[0] = False
 
-        service_finish_time[0] = arrival_time[0] + service_duration[0]
+        for link in paths[current_path]:
+            links[link][1][0] = arrival_time[0] + service_duration[0]
 
         for i in range(1,max_arrival):
             current_path = random.randint(1, 10)
 
-            for j in range(i-1,0,-1):
-                if service_finish_time[j] > arrival_time[i]:
-                    for link in paths[current_path]:
-                        links[link] = links[link] - 1
-
-            for link in paths[current_path]:      
-                if links[link] > 0:
+            for link in paths[current_path]:
+                for j in range(i-1,0,-1):
+                    if links[link][1][j] > arrival_time[i] and links[link][0] > 0:
+                        links[link][0] = links[link][0] - 1
+                        
+                              
+                if links[link][0] > 0:
                     block_accept[i] = False
-                    service_finish_time[i] = arrival_time[i] + service_duration[i]
                     
                 else:
                     block_accept[i] = True
-                    
                     break
+                    
+                links[link][1][i] = arrival_time[i] + service_duration[i]
+            
+
         
-        print(sum(block_accept), lamda)
+        print(sum(block_accept))
         b = sum(block_accept)/max_arrival
 
         bpr.append(b)
@@ -122,10 +125,10 @@ if __name__ == "__main__":
     ax.set(xlabel='offered load(Erlang)', ylabel='system blocking probability',
         title="Blocking Probability of a M/M/k/k system")
 
-    ax.set_yscale('log')
+    # ax.set_yscale('log')
 
-    plt.ylim(10**-6,0.3)
-    plt.xlim(2,20)
+    plt.ylim(0.2,1)
+    plt.xlim(6,k)
     
 
     fig.savefig("results.png")
